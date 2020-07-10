c64d7a0ade6610c9c04ddde2c6e274987943a9c7
Amanrique29 <aitorman90@gmail.com>
2020-07-10 12:31:02 +0200

    buscadorNormalconFilter

--- src/buscador.js
+++ src/buscador.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Link, useParams } from 'react-router-dom';
 
-function Buscador(){
-    
-    function search(){
-        let valor = document.getElementById("buscar").value;
+function Buscador() {
+    let [texto, setTexto] = useState('');
+    let [resultados, setResultados] = useState('');
+    function search() {
+        let valor = document.getElementById("buscar").value.toLowerCase();
         fetch('/buscador').then(function (response) {
             return response.json();
         }).then(function (data) {
-            let prueba = data[0].titulo;
-            console.log(prueba);  
-        })
+            let titulo;
+            let pais;
+            let provincia;
+            let descripcion;
+            let datosFiltrados = [];
+            data.filter(function (datos) {
+                titulo = datos.titulo;
+                pais = datos.pais;
+                provincia = datos.provincia;
+                descripcion = datos.descripcion;
+                if (titulo.toLowerCase().indexOf(valor) !== -1 || pais.toLowerCase().indexOf(valor) !== -1 || provincia.toLowerCase().indexOf(valor) !== -1) {
+                    datosFiltrados.push(datos);
+                }
+            });
+            if (datosFiltrados.length === 0) {
+                setTexto(<p>No hay resultados que mostrar</p>)
+                setResultados('');
+            } else {
+                setResultados(`${datosFiltrados.length} resultados que coinciden con tu búsqueda.`);
+                setTexto(datosFiltrados.map(function (response) {
+                    return (
+                        <>
+                            <h1>{response.titulo}</h1>
+                            <h2>{response.pais}</h2>
+                            <h2>{response.provincia}</h2>
+                            <p>{response.descripcion}</p>
+                        </>
+                    );
+                }));
+            };
+
+        });
     };
-return (
-    <>
-    <input type="text" id="buscar"></input>
-    <button onClick={search}>Buscar</button>
-    </>
-)
+    console.log(texto);
+    return (
+        <>
+            <input type="text" id="buscar"></input>
+            <button onClick={search}>Buscar</button>
+            <p>{resultados}</p>
+            {texto}
+        </>
+    )
 };
 
 export default Buscador;
