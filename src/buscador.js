2e6836f87f375199e06f0e49797b8b2882abbb2a
MariaEscaladaMarcoGardoqui <mescaladamg@gmail.com>
2020-07-21 19:06:28 +0200

    cookies

--- src/buscador.js
+++ src/buscador.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Link, useParams } from 'react-router-dom';
+import {useCookies} from 'react-cookie'
 import './Buscador.css';
 import './DescripcionBuscador.js'
 import DescripcionBuscador from './DescripcionBuscador.js';
